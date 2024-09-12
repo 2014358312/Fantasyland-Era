4f08f36363a909b1e0ed8b3479d8c6530088f874
ConstructionCompleted <143050747+ConstructionCompleted@users.noreply.github.com>
2024-09-12 18:29:28 +0800

    那货张嘴还没说话自己开始笑

--- common/defines/FE_defines.lua
+++ common/defines/FE_defines.lua
@@ -1,3 +1,7 @@
+-- modify game start&end date
 NDefines.NGame.START_DATE = "1000.1.1.12"
 NDefines.NGame.END_DATE = "1030.1.1.1"
+-- modify rules of volunteers
+NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0
+NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0
 NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 0
