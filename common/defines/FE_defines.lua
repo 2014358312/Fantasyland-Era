997258ecb805a99fade81e24fb43bce13e948a82
ConstructionCompleted <143050747+ConstructionCompleted@users.noreply.github.com>
2024-11-15 16:18:36 +0800

    换个风格

--- common/defines/FE_defines.lua
+++ common/defines/FE_defines.lua
@@ -5,3 +5,14 @@ NDefines.NGame.END_DATE = "1030.1.1.1"
 NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0
 NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0
 NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 0
+-- Pax Bratainnica map style
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_DEFAULT_COLOR = { 0.43, 0.22, 0.22, 0.25 }			-- Color of states/provinces that can't be built on
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_MAX_LEVEL_COLOR = { 0.05, 0.1, 0.7, 0.4 } 			-- Color of states/provinces where current building level is maxed out (max is current max level, not final max level) of a building type
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_LEVEL_LOW_COLOR = { 0.05, 0.22, 0.0, 0.4 }
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_LEVEL_HI_COLOR = { 0.4, 0.9, 0.0, 0.5 }
+NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 550.0					-- Remove map objects at this distance
+NDefines.NGraphics.SUN_HEIGHT = 1200
+NDefines.NGraphics.SUN_INTENSITY = 0.9
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0.0
+NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.9
+NDefines.NGraphics.GRADIENT_BORDERS_CAMERA_DISTANCE_OVERRIDE_COUNTRY = 0.08
